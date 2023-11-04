43b882805eb0f28bc030b33549b76cd2efe91037
Your Name <you@example.com>
2023-11-04 20:26:29 +0800

    finish buffer2frame,frame2buffer, wait for test

--- include/Frame.hpp
+++ include/Frame.hpp
@@ -1,6 +1,5 @@
-#include <cstddef>
 #include <iostream>
-#include <string>
+#include <memory>
 
 // 3rd party include
 
@@ -11,7 +10,7 @@ namespace rm_serial_port {
  */
 struct Frame {
   u_int8_t SOF = 0xA5; // 裁判系统中，SOF为0xA5
-  u_int16_t data_length;
+  u_int16_t data_length{0};
   u_int8_t seq;
   u_int8_t CRC8;
   u_int16_t cmd_id;
@@ -38,6 +37,30 @@ struct Frame {
       data[i] = buffer[7 + i];
     }
     CRC16 = static_cast<uint16_t>(buffer[7 + data_length]) << 8 | buffer[8 + data_length];
+  }
+
+  /**
+   * @brief 将帧转换为缓冲区数据
+   * 
+   * @return std::shared_ptr<u_int8_t[]> 字节数组智能指针
+   */
+  std::shared_ptr<u_int8_t[]> frame_to_buffer()
+  {
+    auto buffer = std::make_shared<u_int8_t[]>(data_length + 9);
+    buffer[0] = SOF;
+    buffer[1] = static_cast<uint8_t>(data_length >> 8);
+    buffer[2] = static_cast<uint8_t>(data_length);
+    buffer[3] = seq;
+    buffer[4] = CRC8;
+    buffer[5] = static_cast<uint8_t>(cmd_id >> 8);
+    buffer[6] = static_cast<uint8_t>(cmd_id);
+    for (size_t i = 0; i < data_length; i++)
+    {
+      buffer[7 + i] = data[i];
+    }
+    buffer[7 + data_length] = static_cast<uint8_t>(CRC16 >> 8);
+    buffer[8 + data_length] = static_cast<uint8_t>(CRC16);
+    return buffer;
   }
 
   ~Frame() {
